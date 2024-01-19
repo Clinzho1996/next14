3f51e81b75c468264936d67f819bad61a371b1d9
Confidence Emonena <confidinho@yahoo.com>
2024-01-19 10:04:34 +0100

    next 14

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./about.module.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className={styles.container}>
+      <div></div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/14875250/pexels-photo-14875250.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt="img"
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Dev-Clinton</div>
+      <div className={styles.text}>
+        DC creative thoughts agency © All rights reserved.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
